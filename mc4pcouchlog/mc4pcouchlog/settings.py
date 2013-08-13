f720ee3ac0869c4ea3a7a69f0c0050abd3657d37
Paulson McIntyre <paul@gpmidi.net>
2013-08-13 19:30:19 -0400

    Started on the settings config

--- mc4pcouchlog/mc4pcouchlog/settings.py
+++ mc4pcouchlog/mc4pcouchlog/settings.py
@@ -29,7 +29,7 @@ ALLOWED_HOSTS = []
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
 # although not all choices may be available on all operating systems.
 # In a Windows environment this must be set to your system time zone.
-TIME_ZONE = 'America/Chicago'
+TIME_ZONE = 'America/New_York'
 
 # Language code for this installation. All choices can be found here:
 # http://www.i18nguy.com/unicode/language-identifiers.html
@@ -121,9 +121,14 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     # Uncomment the next line to enable the admin:
-    # 'django.contrib.admin',
+    'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
+    # CouchDB Kit
+    'couchdbkit.ext.django',
+
+    # Us
+    'couchlog',
 )
 
 # A sample logging configuration. The only tangible logging
@@ -154,3 +159,10 @@ LOGGING = {
         },
     }
 }
+
+
+# Load our own, custom settings
+try:
+    from mc4pcouchlog.local_settings import *
+except ImportError, e:
+    pass
