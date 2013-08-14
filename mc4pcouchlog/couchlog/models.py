115583d9ea9455ed5a28ee3ddd70b3fd9892f5b3
Paulson McIntyre <paul@gpmidi.net>
2013-08-13 20:24:39 -0400

    Initial work on a 'zomg log everything' with no analysis (yet)

--- mc4pcouchlog/couchlog/models.py
+++ mc4pcouchlog/couchlog/models.py
@@ -1,3 +1,51 @@
 from django.db import models
+from couchdbkit.ext.django.schema import *
+import datetime
 
-# Create your models here.
+class RawEvent(Document):
+    """ Something that happened """
+    directions=(
+                ('c2s','Client To Server'),
+                ('s2c','Server To Client'),
+                ('other', 'Unknown'),
+                )
+    direction = StringProperty(
+                               required = True,
+                               choices = directions,
+                               verbose_name = "Message Direction",
+                               )
+    eventType = IntegerProperty(
+                                required = True,
+                                verbose_name = "Event Type ID",
+                                )
+    eventTypeName = StringProperty(
+                                required = False,
+                                verbose_name = "Event Type Name",
+                                default = None,
+                                )
+    rawBytes = StringProperty(
+                              required = True,
+                              default = '',
+                              verbose_name = "Raw Data",
+                              )
+    occured = DateTimeProperty(
+                             required = True,
+                             verbose_name = "Event Date/Time",
+                             )
+    otherAttrs = DictProperty(
+                              verbose_name = "Other Props",
+                              required = True,
+                              default = {},
+                              )
+    # Entry info tracking - Pretty useless
+    created = DateTimeProperty(
+                             auto_now_add = True,
+                             required = True,
+                             verbose_name = "Created",
+                             )
+    modified = DateTimeProperty(
+                             auto_now = True,
+                             required = True,
+                             verbose_name = "Last Modified",
+                             )
+    
---
+++ mc4pcouchlog/couchlog/plugin.py
@@ -0,0 +1,57 @@
+'''
+Created on Aug 13, 2013
+
+@author: Paulson McIntyre (GpMidi) <paul@gpmidi.net>
+'''
+import logging
+log = logging.getLogger('couchlog.plugin')
+
+import datetime
+import os, os.path
+import sys
+
+from mc4p.plugins import MC4Plugin
+from mc4p.messages import cli_msgs, srv_msgs
+
+from couchlog.models import *
+
+
+class CouchLogPlugin(MC4Plugin):
+    IGNORE = {
+              'mgstype':True,
+              'raw_bytes':True,
+              }
+    
+    def default_handler(self, msg, source):
+        e = RawEvent()
+        if source == 'server':
+            e.direction = 's2c'
+            msgs = srv_msgs
+        elif source == 'client':
+            e.direction = 'c2s'
+            msgs = cli_msgs
+        else:
+            e.direction = 'other'
+        
+        e.eventType = msg['msgtype']
+        try:
+            et=msgs[msg['msgtype']]
+            e.eventTypeName=et.name
+        except ValueError,e:
+            e.eventTypeName=None
+        
+        e.rawBytes = msg['raw_bytes']
+        e.occured = datetime.datetime.utcnow()
+        
+        try:
+            for k,v in msg.iteritems():
+                try:
+                    if k not in self.IGNORE:
+                        e.otherAttrs[k] = v
+                except Exception, e:
+                    log.exception("Error while setting other args %r:%r", k, v)
+        except Exception,e:
+            log.exception("Error while gathering args from %r/%r",msg,source)
+
+        e.save()
+        return True
